0f90e8517fb145f0cd1c6b9557b8ed5403471308
xov121 <x.o.v121@gmail.com>
2023-10-20 13:23:22 -0400

    initial commit

---
+++ functions_nested_loop/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int _putchar( char c );
+
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0)
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
