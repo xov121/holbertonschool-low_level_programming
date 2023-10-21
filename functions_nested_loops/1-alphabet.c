4fcbd75a348cc52338fea8e702f5f46232897809
xov121 <x.o.v121@gmail.com>
2023-10-21 16:16:10 -0400

    first commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,11 +6,22 @@
  **/
 int main(void)
 {
-
-for (c = 'a'; c <= 'z'; c++)
+print_alphabet();
+return (0);
+}
+/**
+ * print_alphabet - Prints alphabet in lowercase.
+ * Description: This function uses a loop to go through each letter
+ * of the alphabet from 'a' to 'z', and prints each letter to the
+ * standard output using the _putchar function.
+ * Return: This function does not return anything as its return type is void.
+ **/
+void print_alphabet(void)
 {
-_putchar(c);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
 }
 _putchar('\n');
-return (0);
 }
