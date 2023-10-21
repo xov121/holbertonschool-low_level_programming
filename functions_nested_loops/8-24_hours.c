0c94644e95568aaaf8c0457995d4ac23516d0c31
xov121 <x.o.v121@gmail.com>
2023-10-21 19:03:07 -0400

    first commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,6 +10,7 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
+_putchar('0' + hour / 10);
 _putchar('0' + hour % 10);
 _putchar(':');
 _putchar('0' + minute / 10);
