846cc3eea9e5b42c02cdc914e43d667518e03c52
xov121 <x.o.v121@gmail.com>
2023-10-21 15:45:15 -0400

    first commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *Return: Always 0 (Success)
+ **/
+int main(void)
+{
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
