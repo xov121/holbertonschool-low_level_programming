906b156b4dc50a9a69c1ccfc90cb6ed0fbe0a550
xov121 <x.o.v121@gmail.com>
2023-10-21 16:55:11 -0400

    first commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,9 @@ void print_alphabet_x10(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-print_alphabet();
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 }
 _putchar('\n');
 }
