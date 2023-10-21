2c728a54427c314bd13afa7926b9e2b37bd79f42
xov121 <x.o.v121@gmail.com>
2023-10-21 18:06:32 -0400

    first commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,14 +1,26 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the number to compute the absolute value for
- * Return: the absolute value of n
- **/
-int _abs(int n)
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ */
+int print_sign(int n)
 {
-if (n < 0)
-return (-n);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
 else
-return (n);
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
 }
