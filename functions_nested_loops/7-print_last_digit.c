71d930daa589916d7755bba91e4cc8d475b4edf0
xov121 <x.o.v121@gmail.com>
2023-10-21 18:37:42 -0400

    first commit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: the number whose last digit is to be printed and returned
+ * Return: returns last digit
+ **/
+int print_last_digit(int n)
+{
+int lastD = n % 10;
+_putchar('0' + lastD);
+return (lastD);
+}
