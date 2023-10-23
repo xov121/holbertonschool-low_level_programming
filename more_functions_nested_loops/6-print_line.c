2c46ef9a734b2ff938936266548464111e4c4f15
xov121 <x.o.v121@gmail.com>
2023-10-23 09:25:07 -0400

    first commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - prints _ n number of times
+* @n: how many times to print _
+* Return: void
+**/
+void print_line(int n)
+{
+int n;
+if (n > 0)
+{
+_putchar('_');
+}
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('\n');
+}
