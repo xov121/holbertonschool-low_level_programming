edd329662cb238fd9d07419b514d016215f265d5
xov121 <x.o.v121@gmail.com>
2023-10-23 14:31:51 -0400

    first commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 int n;
-
 for (n = 1; n <= 100; n++)
 {
 if ((n % 3) == 0 && (n % 5) == 0)
@@ -16,7 +15,7 @@ if ((n % 3) == 0 && (n % 5) == 0)
 printf("FizzBuzz");
 }
 else
-if ((n % 3) == 0) 
+if ((n % 3) == 0)
 {
 printf("Fizz");
 }
