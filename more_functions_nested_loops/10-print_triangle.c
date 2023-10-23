96c64edd70f99f1701a8e99af336b77cda8ca4fe
xov121 <x.o.v121@gmail.com>
2023-10-23 16:48:52 -0400

    first commit

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,13 +12,16 @@ if (size <= 0)
 _putchar('\n');
 return;
 }
-for (int i = 1; i <= size; ++i)
+int i;
+for (i = 1; i <= size; ++i)
 {
-for (int j = 1; j <= size - i; ++j)
+int j;
+for (j = 1; j <= size - i; ++j)
 {
 _putchar(' ');
 }
-for (int k = 1; k <= i; ++k)
+int k;
+for (k = 1; k <= i; ++k)
 {
 _putchar('#');
 }
