d484e71e349bfb7944d3878afe226964660dd3fb
xov121 <x.o.v121@gmail.com>
2023-10-23 13:18:00 -0400

    first commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_square - function to print # square
+* @size: size of square
+* Return: void
+**/
+void print_square(int size)
+{
+if (size > 0)
+{
+int i = 1;
+while (i <= size)
+{
+int j = 1;
+while (j <= size)
+{
+_putchar('#');
+j++;
+}
+_putchar('\n');
+i++;
+}
+_putchar('\n');
+}
+}
