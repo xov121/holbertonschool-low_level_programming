12a3a05dfc0877e5c93364906f82b57d0e018347
xov121 <x.o.v121@gmail.com>
2023-10-22 23:23:18 -0400

    first commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 1 through 9
+ * Return: nothing, it's void
+ **/
+
+void print_numbers(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
