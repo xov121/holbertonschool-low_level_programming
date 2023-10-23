ea4081329f8b8ed15821e684c3d4cdca8c354abb
xov121 <x.o.v121@gmail.com>
2023-10-23 13:59:02 -0400

    first commit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+int n;
+
+for (n = 1; n <= 100; n++)
+{
+if ((n % 3) == 0 && (n % 5) == 0)
+{
+printf("FizzBuzz ");
+}
+else
+if ((n % 3) == 0) 
+{
+printf("Fizz ");
+}
+else
+if ((n % 5) == 0)
+{
+printf("Buzz ");
+}
+else
+printf("%i ", n);
+}
+printf("\n");
+}
