a14215afc8ce301f882dab000e60eada86301012
xov121 <x.o.v121@gmail.com>
2023-10-24 13:24:05 -0400

    first commit

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps value of 2 integers
+ * @a: first value
+ * @b: second value
+ * Return: void
+ **/
+void swap_int(int *a, int *b)
+{
+int temp = *a
+a = b;
+b = temp;
+}
