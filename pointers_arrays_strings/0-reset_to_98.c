eccaa88e62cda92c3b5406b7f8abe7d7a13d9ddb
xov121 <x.o.v121@gmail.com>
2023-10-24 10:34:34 -0400

    first commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* reset_to_98 - updates value of pointer
+*@n: variable it points
+**/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+_putchar(char c);
+void reset_to_98(int *n);
