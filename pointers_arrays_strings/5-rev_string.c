9fe6c8b229e1ce6905660915437f7af5f37269f8
xov121 <x.o.v121@gmail.com>
2023-10-25 16:21:52 -0400

    first commit

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string in place.
+ * @s: The string to be reversed.
+ **/
+void rev_string(char *s)
+{
+int len = 0;
+int i;
+char temp;
+while (s[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
+}
