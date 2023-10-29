455b66b94cf2ae7d8b700cc07920c49f7847cb4d
xov121 <x.o.v121@gmail.com>
2023-10-29 19:30:23 -0400

    first commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,31 @@
 #include "main.h"
 
 /**
- * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * cap_string - Capitalizes all words of a string.
  * @s: The string to be modified.
  * Return: A pointer to the modified string.
  **/
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
 int i = 0;
+int new_word = 1;
 while (s[i] != '\0')
 {
-if (s[i] >= 'a' && s[i] <= 'z')
+if (new_word && (s[i] >= 'a' && s[i] <= 'z'))
 {
 s[i] = s[i] - 'a' + 'A';
 }
+if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' ||
+s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' ||
+s[i] == ')' || s[i] == '{' || s[i] == '}')
+{
+new_word = 1;
+}
+else
+{
+new_word = 0;
+}
 i++;
 }
-return s;
+return (s);
 }
