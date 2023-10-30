3ac1b744a79bff97ffa057769f05f2c0f5f89254
xov121 <x.o.v121@gmail.com>
2023-10-29 20:23:19 -0400

    first commit

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  *@s: The string to be scanned
  *@accept: The string containing the characters to match
  *Return: The number of bytes in the initial segment of s
-          which consist only of bytes from accept
+ *which consist only of bytes from accept
  **/
 unsigned int _strspn(char *s, char *accept)
 {
