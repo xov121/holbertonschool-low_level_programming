a857980974b26ba04663f3a5872e5e01c01caf8f
xov121 <x.o.v121@gmail.com>
2023-10-29 20:05:06 -0400

    first commit

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,10 +9,11 @@
  **/
 char *_memset(char *s, char b, unsigned int n)
 {
-*ptr = b;
+unsigned int i;
 char *ptr = s;
-for (unsigned int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
+*ptr = b;
 ptr++;
 }
 return (s);
