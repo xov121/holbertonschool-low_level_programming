c7b9b299eb273b75f83d76523ba643c5c06f7856
xov121 <x.o.v121@gmail.com>
2023-11-20 08:48:41 -0400

    first commit

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicates a string
+ * @str: The string to duplicate
+ * Return: Pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int len = 0;
+while (str[len])
+len++;
+dup = malloc(len + 1);
+if (dup == NULL)
+return (NULL);
+for (unsigned int i = 0; i < len; i++)
+dup[i] = str[i];
+dup[len] = '\0';
+return (dup);
+}
+/**
+ * new_dog - Creates a new dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: Pointer to the new dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+dog = malloc(sizeof(dog_t));
+if (!dog)
+return (NULL);
+dog->name = _strdup(name);
+if (!dog->name)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = _strdup(owner);
+if (!dog->owner)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+dog->age = age;
+return (dog);
+}
