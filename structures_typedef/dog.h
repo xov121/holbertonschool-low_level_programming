0bf1a17483ff1119e0f58de324ed766928b8ab11
xov121 <x.o.v121@gmail.com>
2023-11-20 00:30:03 -0400

    first commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+
+/**
+ * struct dog - a new type describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
