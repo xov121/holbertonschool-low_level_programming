2c51465d399c361df7a636e27581af7e389f8d9d
xov121 <x.o.v121@gmail.com>
2023-11-20 01:19:55 -0400

    first commit

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints dog structure
+ * @d: pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.2f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
