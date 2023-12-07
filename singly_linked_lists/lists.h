bc8d01b6603edba745a7d2f89ef7175c5a0d096e
xov121 <x.o.v121@gmail.com>
2023-12-07 01:20:31 -0400

    Fixed betty indentation.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -8,14 +8,14 @@
  * @str: string - (nalloc'ed string).
  * @len: lenght of the string.
  * @next: points to the next node.
- * 
+ *
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-}list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 /* Function prototypes */
 size_t print_list(const list_t *h);
