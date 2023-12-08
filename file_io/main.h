bdb34e1d08677f5f588c7a5d81cfa44a20bc1278
xov121 <x.o.v121@gmail.com>
2023-12-08 03:55:06 -0400

    Included 'sys/types.h' for ssize_t and 'stddef.h' for size_t.

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <stddef.h>
+
 /* Prototype */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
