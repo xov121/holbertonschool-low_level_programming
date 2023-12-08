12a94641cffd18649b59a32fc5cab850496fcba2
xov121 <x.o.v121@gmail.com>
2023-12-08 04:18:25 -0400

    Creates a file.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * create_file - Creates a file.
+ * @filename: The name of the file to be created.
+ * @text_content: A NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0, written;
+
+	if (!filename)
+	return (-1);
+
+	fd = open(filename, o_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+		len++;
+		written = write(fd, text_content, len);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
