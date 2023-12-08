342b1210f9efdfb6da8c57ac95d9881a6ad7ddb9
xov121 <x.o.v121@gmail.com>
2023-12-08 01:55:15 -0400

    Fixed more typos related to 'uint' inside the code

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * binary_to_unit - Converts a binary number to an unsigned int.
+ * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: Pointer to a string of 0 and 1 chars.
  *
  * Return: The converted number or 0 if:
  * - there is one or more chars in the string b that is not 0 or 1.
  * - b is NULL.
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 	int i;
