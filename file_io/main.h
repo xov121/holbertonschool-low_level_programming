279d955516d48df85c007bf57134b5e595ef9ddc
xov121 <x.o.v121@gmail.com>
2023-12-10 18:37:13 -0400

    Added prototypes and buffer size definition for '3-cp.c'.

--- file_io/main.h
+++ file_io/main.h
@@ -3,10 +3,18 @@
 
 #include <sys/types.h>
 #include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
 
 /* Prototype */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_file(int fd);
+void print_error(char *msg, char *file, int status);
 
 #endif /* MAIN_H */
